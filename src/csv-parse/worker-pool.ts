dbcc59e49444f274474e0d7cf00288bb64de04d0
Tyler Whited <tylerwhited@tylers-mbp-2.lan>
2021-05-31 09:32:34 -0600

    Move worker thread functionality into functions

--- src/csv-parse/worker-pool.ts
+++ src/csv-parse/worker-pool.ts
@@ -67,7 +67,6 @@ enum MESSAGE_TYPES {
   CONVERT_CSV_LOG_COMPLETE = 'convert_csv_log_complete',
   CONVERT_CSV_LOG_RECORD_READ = 'convert_csv_log_record_read',
   CSV_WRITER = 'csv_writer',
-  CSV_WRITER_INIT = 'csv_writer_init',
   CSV_WRITER_WRITE = 'csv_writer_write',
   CSV_WRITER_WRITE_DONE = 'csv_writer_write_done',
   CSV_WRITER_END = 'csv_writer_end',
@@ -650,51 +649,16 @@ function checkQueues(queueLoop?: boolean) {
 }
 
 function initWorkerThread() {
-  let recordCb: (record: any[], idx: number) => void;
   parentPort.on('message', (msg: WorkerMessage) => {
     switch(msg.messageType) {
       case MESSAGE_TYPES.ACK:
-        parentPort.postMessage({
-          messageType: MESSAGE_TYPES.ACK,
-        });
+        handleAck(msg);
         break;
       case MESSAGE_TYPES.PARSE_CSV:
-        const csvPath = (msg?.data?.csvPath as string);
-        if(csvPath === undefined) {
-          console.error('Error, malformed message sent to worker:');
-          console.error(msg);
-        } else {
-          sleep(0).then(() => {
-            return parseCsvLog(csvPath)
-              .then(parseResult => {
-                parentPort.postMessage({
-                  messageType: MESSAGE_TYPES.PARSE_CSV_COMPLETE,
-                  data: {
-                    parseResult,
-                    csvPath,
-                  },
-                });
-              });
-          });
-        }
+        handleParseCsv(msg);
         break;
       case MESSAGE_TYPES.CONVERT_CSV:
-        const csvPathDate = (msg?.data?.csvPathDate as CsvPathDate);
-        if(csvPathDate === undefined) {
-          console.error('Error, malformed message sent to worker:');
-          console.error(msg);
-        } else {
-          convertCsvPathDate(csvPathDate)
-            .then(convertResult => {
-              parentPort.postMessage({
-                messageType: MESSAGE_TYPES.CONVERT_CSV_COMPLETE,
-                data: {
-                  csvPathDate,
-                  convertResult,
-                }
-              });
-            });
-        }
+        hanldeConvertCsv(msg);
         break;
       /*
 
@@ -703,54 +667,10 @@ function initWorkerThread() {
 
       */
       case MESSAGE_TYPES.CSV_READ:
-        const readCsvPath = (msg?.data?.csvPath as string);
-        const recordReadQueue: any[][] = [];
-        const clearReadQueue = () => {
-          parentPort.postMessage({
-            messageType: MESSAGE_TYPES.ASYNC_CSV_READ,
-            data: {
-              csvPath: readCsvPath,
-              records: recordReadQueue,
-            }
-          });
-          recordReadQueue.length = 0;
-        };
-        recordCb = (record, idx) => {
-          recordReadQueue.push(record);
-          if(recordReadQueue.length > ASYNC_READ_RECORD_QUEUE_SIZE) {
-            clearReadQueue();
-          }
-        };
-        if(readCsvPath === undefined) {
-          console.error('Error, malformed message sent to worker:');
-          console.error(msg);
-        } else {
-          readCsvLog(readCsvPath, recordCb).then(csvReadResult => {
-            clearReadQueue();
-            parentPort.postMessage({
-              messageType: MESSAGE_TYPES.CSV_READ_COMPLETE,
-              data: {
-                csvPath: readCsvPath,
-                readResult: csvReadResult,
-              },
-            });
-          });
-        }
+        handleCsvRead(msg);
         break;
       case MESSAGE_TYPES.CSV_WRITER:
-        const csvWriterPath = (msg?.data?.csvPath as string);
-        if(csvWriterPath === undefined) {
-          console.error('Error, malformed message sent to worker:');
-          console.error(msg);
-        } else {
-          asyncCsvWriter = new CsvWriter(csvWriterPath);
-          parentPort.postMessage({
-            messageType: MESSAGE_TYPES.CSV_WRITER,
-            data: {
-              csvPath: csvWriterPath,
-            },
-          })
-        }
+        handleCsvWriter(msg);
         break;
       /*
 
@@ -758,139 +678,249 @@ function initWorkerThread() {
       v v v v v v v v v v v v v v v v v v v v v
 
       */
-      case MESSAGE_TYPES.CSV_WRITER_INIT:
-        const asyncCsvWriterPath = (msg?.data?.filePath as string);
-        if(asyncCsvWriterPath === undefined) {
-          console.error('Error, malformed message sent to worker:');
-          console.error(msg);
-        } else {
-          asyncCsvWriter = new CsvWriter(asyncCsvWriterPath);
-          parentPort.postMessage({
-            messageType: MESSAGE_TYPES.CSV_WRITER_INIT,
-          });
-        }
-        break;
       case MESSAGE_TYPES.CSV_WRITER_END:
-        if(asyncCsvWriter !== undefined) {
-          (async () => {
-            await asyncCsvWriter.end();
-            asyncCsvWriter = undefined;
-            parentPort.postMessage({
-              messageType: MESSAGE_TYPES.CSV_WRITER_END,
-            });
-          })();
-        }
-        break;
+        handleCsvWriterEnd(msg);
+        break
       case MESSAGE_TYPES.CSV_WRITER_WRITE:
-        const asyncCsvWriteRecords = (msg?.data?.records as any[][]);
-        if(!Array.isArray(asyncCsvWriteRecords)) {
-          console.error('Error, malformed message sent to worker:');
-          console.log(msg?.messageType);
-        } else {
-          (async () => {
-            // while(asyncCsvWriteRecords.length > 0) {
-            //   await asyncCsvWriter.write(asyncCsvWriteRecords.pop());
-            // }
-            for(let i = 0; i < asyncCsvWriteRecords.length; ++i) {
-              await asyncCsvWriter.write(asyncCsvWriteRecords[i]);
-            }
-            parentPort.postMessage({
-              messageType: MESSAGE_TYPES.CSV_WRITER_WRITE_DONE,
-            });
-          })();
-        }
-        // asyncCsvWriter.write();
+        handleCsvWriterWrite(msg);
         break;
       case MESSAGE_TYPES.CONVERT_CSV_LOG:
-        const filePath = (msg?.data?.filePath as string);
-        if(filePath === undefined) {
-          console.error('Error, malformed message sent to worker:');
-          console.error(msg);
-        } else {
-          const recordQueue: any[][] = [];
-          const clearRecordQueue = () => {
-            let recordQueueCopy: any[][];
-            recordQueueCopy = recordQueue.slice();
-            parentPort.postMessage({
-              messageType: MESSAGE_TYPES.CONVERT_CSV_LOG_RECORD_READ,
-              data: {
-                filePath,
-                records: recordQueueCopy,
-              },
-            });
-            recordQueue.length = 0;
-          };
-          const checkClear = () => {
-            if(recordQueue.length > ASYNC_READ_RECORD_QUEUE_SIZE) {
-              return clearRecordQueue();
-            }
-          };
-          const recordCb = (record: any[]) => {
-            recordQueue.push(record);
-            checkClear();
-          };
-          sleep(0).then(() => {
-            return convertCsvLogFile(filePath, recordCb)
-              .then(convertLogResult => {
-                if(recordQueue.length > 0) {
-                  clearRecordQueue();
-                }
-                return convertLogResult;
-              })
-              .then(convertLogResult => {
-                parentPort.postMessage({
-                  messageType: MESSAGE_TYPES.CONVERT_CSV_LOG_COMPLETE,
-                  data: {
-                    filePath,
-                    convertLogResult,
-                  },
-                });
-              });
-          });
-        }
+        handleConvertCsvLog(msg);
         break;
       case MESSAGE_TYPES.HASH_FILE:
-        const hashFilePath = (msg?.data?.filePath as string);
-        if(hashFilePath === undefined) {
-          console.error('Error, malformed message sent to worker:');
-          console.error(msg);
-        } else {
-          getFileHash(hashFilePath)
-            .then(fileHash => {
-              parentPort.postMessage({
-                messageType: MESSAGE_TYPES.HASH_FILE_COMPLETE,
-                data: {
-                  filePath: hashFilePath,
-                  fileHash,
-                },
-              });
-            });
-        }
+        handleHashFile(msg);
         break;
       case MESSAGE_TYPES.CSV_WRITE:
-        const targetCsvPath = (msg?.data?.targetCsvPath as string);
-        const rows = (msg?.data?.rows as any[][]);
-        if((targetCsvPath === undefined) || rows === undefined) {
-          console.error('Error, malformed message sent to worker:');
-          console.error(`messageType: ${msg?.messageType}`);
-          console.error(`targetCsvPath: ${msg?.data?.targetCsvPath}`);
-        } else {
-          writeCsv(targetCsvPath, rows)
-            .then(() => {
-              parentPort.postMessage({
-                messageType: MESSAGE_TYPES.CSV_WRITE_COMPLETE,
-                data: {
-                  targetCsvPath,
-                },
-              });
-            });
-        }
+        handleCsvWrite(msg);
         break;
       case MESSAGE_TYPES.EXIT:
-        setTimeout(() => {
-          process.exit();
-        });
+        handleExit(msg);
         break;
     }
+  });
+}
+
+function handleAck(msg: WorkerMessage) {
+  parentPort.postMessage({
+    messageType: MESSAGE_TYPES.ACK,
+  });
+}
+
+function handleParseCsv(msg: WorkerMessage) {
+  const csvPath = (msg?.data?.csvPath as string);
+  if(csvPath === undefined) {
+    console.error('Error, malformed message sent to worker:');
+    console.error(msg);
+  } else {
+    sleep(0).then(() => {
+      return parseCsvLog(csvPath)
+        .then(parseResult => {
+          parentPort.postMessage({
+            messageType: MESSAGE_TYPES.PARSE_CSV_COMPLETE,
+            data: {
+              parseResult,
+              csvPath,
+            },
+          });
+        });
+    });
+  }
+}
+
+function hanldeConvertCsv(msg: WorkerMessage) {
+  const csvPathDate = (msg?.data?.csvPathDate as CsvPathDate);
+  if(csvPathDate === undefined) {
+    console.error('Error, malformed message sent to worker:');
+    console.error(msg);
+  } else {
+    convertCsvPathDate(csvPathDate)
+      .then(convertResult => {
+        parentPort.postMessage({
+          messageType: MESSAGE_TYPES.CONVERT_CSV_COMPLETE,
+          data: {
+            csvPathDate,
+            convertResult,
+          }
+        });
+      });
+  }
+}
+
+function handleCsvRead(msg: WorkerMessage) {
+  let recordCb: (record: any[], idx: number) => void;
+  const readCsvPath = (msg?.data?.csvPath as string);
+  const recordReadQueue: any[][] = [];
+  const clearReadQueue = () => {
+    parentPort.postMessage({
+      messageType: MESSAGE_TYPES.ASYNC_CSV_READ,
+      data: {
+        csvPath: readCsvPath,
+        records: recordReadQueue,
+      }
+    });
+    recordReadQueue.length = 0;
+  };
+  recordCb = (record, idx) => {
+    recordReadQueue.push(record);
+    if(recordReadQueue.length > ASYNC_READ_RECORD_QUEUE_SIZE) {
+      clearReadQueue();
+    }
+  };
+  if(readCsvPath === undefined) {
+    console.error('Error, malformed message sent to worker:');
+    console.error(msg);
+  } else {
+    readCsvLog(readCsvPath, recordCb).then(csvReadResult => {
+      clearReadQueue();
+      parentPort.postMessage({
+        messageType: MESSAGE_TYPES.CSV_READ_COMPLETE,
+        data: {
+          csvPath: readCsvPath,
+          readResult: csvReadResult,
+        },
+      });
+    });
+  }
+}
+
+function handleCsvWriter(msg: WorkerMessage) {
+  const csvWriterPath = (msg?.data?.csvPath as string);
+  if(csvWriterPath === undefined) {
+    console.error('Error, malformed message sent to worker:');
+    console.error(msg);
+  } else {
+    asyncCsvWriter = new CsvWriter(csvWriterPath);
+    parentPort.postMessage({
+      messageType: MESSAGE_TYPES.CSV_WRITER,
+      data: {
+        csvPath: csvWriterPath,
+      },
+    })
+  }
+}
+
+function handleCsvWriterEnd(msg: WorkerMessage) {
+  if(asyncCsvWriter !== undefined) {
+    (async () => {
+      await asyncCsvWriter.end();
+      asyncCsvWriter = undefined;
+      parentPort.postMessage({
+        messageType: MESSAGE_TYPES.CSV_WRITER_END,
+      });
+    })();
+  }
+}
+
+function handleCsvWriterWrite(msg: WorkerMessage) {
+  const asyncCsvWriteRecords = (msg?.data?.records as any[][]);
+  if(!Array.isArray(asyncCsvWriteRecords)) {
+    console.error('Error, malformed message sent to worker:');
+    console.log(msg?.messageType);
+  } else {
+    (async () => {
+      // while(asyncCsvWriteRecords.length > 0) {
+      //   await asyncCsvWriter.write(asyncCsvWriteRecords.pop());
+      // }
+      for(let i = 0; i < asyncCsvWriteRecords.length; ++i) {
+        await asyncCsvWriter.write(asyncCsvWriteRecords[i]);
+      }
+      parentPort.postMessage({
+        messageType: MESSAGE_TYPES.CSV_WRITER_WRITE_DONE,
+      });
+    })();
+  }
+}
+
+function handleConvertCsvLog(msg: WorkerMessage) {
+  const filePath = (msg?.data?.filePath as string);
+  if(filePath === undefined) {
+    console.error('Error, malformed message sent to worker:');
+    console.error(msg);
+  } else {
+    const recordQueue: any[][] = [];
+    const clearRecordQueue = () => {
+      let recordQueueCopy: any[][];
+      recordQueueCopy = recordQueue.slice();
+      parentPort.postMessage({
+        messageType: MESSAGE_TYPES.CONVERT_CSV_LOG_RECORD_READ,
+        data: {
+          filePath,
+          records: recordQueueCopy,
+        },
+      });
+      recordQueue.length = 0;
+    };
+    const checkClear = () => {
+      if(recordQueue.length > ASYNC_READ_RECORD_QUEUE_SIZE) {
+        return clearRecordQueue();
+      }
+    };
+    const recordCb = (record: any[]) => {
+      recordQueue.push(record);
+      checkClear();
+    };
+    sleep(0).then(() => {
+      return convertCsvLogFile(filePath, recordCb)
+        .then(convertLogResult => {
+          if(recordQueue.length > 0) {
+            clearRecordQueue();
+          }
+          return convertLogResult;
+        })
+        .then(convertLogResult => {
+          parentPort.postMessage({
+            messageType: MESSAGE_TYPES.CONVERT_CSV_LOG_COMPLETE,
+            data: {
+              filePath,
+              convertLogResult,
+            },
+          });
+        });
+    });
+  }
+}
+
+function handleHashFile(msg: WorkerMessage) {
+  const hashFilePath = (msg?.data?.filePath as string);
+  if(hashFilePath === undefined) {
+    console.error('Error, malformed message sent to worker:');
+    console.error(msg);
+  } else {
+    getFileHash(hashFilePath)
+      .then(fileHash => {
+        parentPort.postMessage({
+          messageType: MESSAGE_TYPES.HASH_FILE_COMPLETE,
+          data: {
+            filePath: hashFilePath,
+            fileHash,
+          },
+        });
+      });
+  }
+}
+
+function handleCsvWrite(msg: WorkerMessage) {
+  const targetCsvPath = (msg?.data?.targetCsvPath as string);
+  const rows = (msg?.data?.rows as any[][]);
+  if((targetCsvPath === undefined) || rows === undefined) {
+    console.error('Error, malformed message sent to worker:');
+    console.error(`messageType: ${msg?.messageType}`);
+    console.error(`targetCsvPath: ${msg?.data?.targetCsvPath}`);
+  } else {
+    writeCsv(targetCsvPath, rows)
+      .then(() => {
+        parentPort.postMessage({
+          messageType: MESSAGE_TYPES.CSV_WRITE_COMPLETE,
+          data: {
+            targetCsvPath,
+          },
+        });
+      });
+  }
+}
+
+function handleExit(msg: WorkerMessage) {
+  setTimeout(() => {
+    process.exit();
   });
 }
